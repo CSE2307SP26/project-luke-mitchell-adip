5cc084521634c3331b7e5f421ec0dd98af195d6d
adipstha11 <adipstha11@gmail.com>
2026-03-23 18:58:53 -0500

    Finished withdraW UI

--- src/main/MainMenu.java
+++ src/main/MainMenu.java
@@ -4,8 +4,8 @@ import java.util.Scanner;
 
 public class MainMenu {
 
-    private static final int EXIT_SELECTION = 2;
-	private static final int MAX_SELECTION = 2;
+    private static final int EXIT_SELECTION = 3;
+	private static final int MAX_SELECTION = 3;
 
 	private BankAccount userAccount;
     private Scanner keyboardInput;
@@ -19,7 +19,8 @@ public class MainMenu {
         System.out.println("Welcome to the 237 Bank App!");
         
         System.out.println("1. Make a deposit");
-        System.out.println("2. Exit the app");
+        System.out.println("2. Make a withdrawal");
+        System.out.println("3. Exit the app");
 
     }
 
@@ -36,6 +37,10 @@ public class MainMenu {
         switch (selection) {
             case 1:
                 performDeposit();
+                break;
+            case 2:
+                performWithdraw();
+                break;
         }
     }
 
@@ -47,7 +52,19 @@ public class MainMenu {
         }
         userAccount.deposit(depositAmount);
     }
+    public void performWithdraw() {
+        double withdrawAmount = -1;
+        while (withdrawAmount < 0) {
+            System.out.print("How much would you like to withdraw: ");
+            withdrawAmount = keyboardInput.nextInt();
+        }
 
+        try {
+            userAccount.withdraw(withdrawAmount);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Invalid withdrawal amount or insufficient funds.");
+        }
+    }
     public void run() {
         int selection = -1;
         while(selection != EXIT_SELECTION) {
